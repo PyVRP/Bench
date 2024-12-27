64466bd11fadaa66738f5b7055babdeb5843a986
Leon Lan <l.lan@vu.nl>
2024-12-27 17:29:05 +0100

    Install from main default; rename commit to revision

--- bench/install.py
+++ bench/install.py
@@ -23,27 +23,25 @@ def uninstall():
     subprocess.check_call(cmd)
 
 
-def install(commit: str):
+def install(revision: str = "main"):
     """
-    Installs a specific commit of PyVRP from GitHub.
+    Installs a specific revision of PyVRP from GitHub.
     """
-    url = f"git+https://github.com/PyVRP/PyVRP@{commit}#egg=PyVRP"
+    url = f"git+https://github.com/pyvrp/pyvrp@{revision}#egg=pyvrp"
     cmd = [sys.executable, "-m", "pip", "install", url]
     subprocess.check_call(cmd)
 
 
 def setup_parser(subparsers):
-    msg = "Install a specific commit of PyVRP from GitHub."
+    msg = "Install the PyVRP solver."
     parser = subparsers.add_parser("install", description=msg, help=msg)
 
-    msg = "Commit hash to install from PyVRP's GitHub repository."
-    parser.add_argument("commit", type=str, help=msg)
-
-    # TODO install main by default?
+    msg = "Revision to install from PyVRP's GitHub repository."
+    parser.add_argument("--revision", default="main", type=str, help=msg)
 
 
-def main(commit: str, **kwargs):
+def main(revision: str, **kwargs):
     if is_installed():
         uninstall()
 
-    install(commit)
+    install(revision)
